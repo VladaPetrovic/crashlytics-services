3bd724153c4403ce8fe3a76162c1e5ca5df36add
Vlada Petrovic <vlada.petrovic@gmail.com>
2013-09-10 18:36:29 +0200

    Fix coding style using rubocop & reek

--- lib/services/databox.rb
+++ lib/services/databox.rb
@@ -1,50 +1,51 @@
+# encoding: utf-8
+
+# The Service::Databox is a class responsible for handling Databox.com
+# Connection settings and acting in response to events it receives from
+# Crashlytics
 class Service::Databox < Service::Base
-    title 'Databox'
+  title 'Databox'
 
-    string :push_data_url, :placeholder => 'https://app.databox.com/service',
-           :label => 'URL for your Crashlytics service'
-    string :api_key, :placeholder => 'API key',
-           :label => 'API key for your Crashlytics service'
+  string :push_url, label: 'URL'
+  string :push_token, label: 'Token'
 
-    page 'Setup', [:push_data_url, :api_key]
+  page 'Push Connection Settings', [:push_url, :push_token]
 
-    def receive_verification(config, _)
-        begin
-            url = "#{config[:push_data_url]}/logs"
-            http.ssl[:verify] = false
-            http.basic_auth config[:api_key], ''
+  def receive_verification(config, _)
+    url               = "#{config[:push_url]}/logs"
+    http.ssl[:verify] = false
+    http.basic_auth config[:push_token], ''
 
-            resp = http_get url
-            if resp.status == 200
-                [true,  'Successfully verified Databox settings!']
-            else
-                log "Databox HTTP Error, status code: #{ resp.status }, body: #{ resp.body }"
-                [false, "Oops! Please check your settings again."]
-            end
-        rescue => e
-            log "Rescued a verification error in Databox: (url=#{config[:push_data_url]}/logs) #{e}"
-            [false, "Oops! Please check your settings again."]
-        end
+    resp = http_get url
+    if resp.status == 200
+      [true,  'Successfully verified Databox connection!']
+    else
+      log "Databox HTTP Error, status code: #{resp.status}, body: #{resp.body}"
+      [false, 'Oops! Please check your settings again.']
     end
+  rescue => exception
+    log "Rescued a verification error in Databox: (url=#{url}) #{exception}"
+    [false, 'Oops! Please check your settings again.']
+  end
 
-    # Push data to Databox
-    def receive_issue_impact_change(config, payload)
-        http.ssl[:verify] = false
-        http.basic_auth config[:api_key], ""
+  # Push data to Databox
+  def receive_issue_impact_change(config, payload)
+    http.ssl[:verify] = false
+    http.basic_auth config[:push_token], ''
 
-        post_body = {
-            :data => [
-                { :key => 'impacted_devices_count', :value => payload[:impacted_devices_count]},
-                { :key => 'crashes_count', :value => payload[:crashes_count]}
-            ]
-        }
+    post_body = {
+      data: [
+        { key: 'impacted_devices_count', value: payload[:impacted_devices_count] },
+        { key: 'crashes_count', value: payload[:crashes_count] }
+      ]
+    }
 
-        resp = http_post config[:push_data_url] do |req|
-            req.body = post_body.to_json
-        end
-        if resp.status != 200
-            raise "Pushing data to Databox Failed: #{ resp[:status] }, #{ resp.body }"
-        end
-        :no_resource
+    resp = http_post config[:push_url] do |req|
+      req.body = post_body.to_json
     end
+    if resp.status != 200
+      raise "Pushing data to Databox Failed: #{resp[:status]}, #{resp.body}"
+    end
+      :no_resource
+  end
 end
--- spec/services/databox_spec.rb
+++ spec/services/databox_spec.rb
@@ -1,120 +1,124 @@
+# encoding: utf-8
+
 require 'spec_helper'
 
 describe Service::Databox do
-	let(:testdata) do
+  let(:base_url)         { 'https://app.databox.com' }
+  let(:service_push_url) { '/push/custom/3rglns26g76sws04' }
+  let(:fixtures) do
     {
-		:push_data_url => 'https://app.databox.com/push/custom/3rglns26g76sws04',
-		:api_key => '5dc5qvbnb9wcwogww8w0g8g8scgo4swg'
+      config: {
+        push_url:   "#{base_url}#{service_push_url}",
+        push_token: '5dc5qvbnb9wcwogww8w0g8g8scgo4swg'
+      },
+      response: {
+        response: {
+          type: 'success',
+          message: 'Items stored: 1'
+        }.to_json
+      }
     }
-	end
+  end
 
-	it 'should have a title' do
-		Service::Databox.title.should == 'Databox'
-	end
+  it 'should have a title' do
+    Service::Databox.title.should == 'Databox'
+  end
 
-	it 'should require one page of information' do
-		Service::Databox.pages.should == [
-			{ :title => 'Setup', :attrs => [:push_data_url, :api_key] }
-		]
-	end
+  it 'should require one page of information' do
+    Service::Databox.pages.should == [
+      { title: 'Push Connection Settings', attrs: [:push_url, :push_token] }
+    ]
+  end
 
-	describe :receive_verification do
-		let(:service) { Service::Databox.new('log', {}) }
-    	let(:config) do
-    	{
-    		:api_key => testdata[:api_key],
-    		:push_data_url => testdata[:push_data_url]
-    	}
-    	end
-  		let(:payload) { {} }
+  describe :receive_verification do
+    let(:service) { Service::Databox.new('logs', {}) }
+    let(:payload) { {} }
 
-		it 'should respond' do
-			service.respond_to?(:receive_verification)
-		end
+    it 'should respond' do
+      service.respond_to?(:receive_verification)
+    end
 
-		it 'should succeed upon successful api response' do
-			test = Faraday.new do |builder|
-				builder.adapter :test do |stub|
-					stub.get('/push/custom/3rglns26g76sws04') { [200, {}, ''] }
-				end
-			end
+    it 'should succeed upon successful API response' do
+      test = Faraday.new do |builder|
+        builder.adapter :test do |stub|
+          stub.get(service_push_url) { [200, {}, ''] }
+        end
+      end
 
-			service.should_receive(:http_get)
-				.with('https://app.databox.com/push/custom/3rglns26g76sws04/logs')
-				.and_return(test.get('/push/custom/3rglns26g76sws04'))
+      service.should_receive(:http_get)
+        .with("#{fixtures[:config][:push_url]}/logs")
+        .and_return(test.get(service_push_url))
 
-			resp = service.receive_verification(config, payload)
-			resp.should == [true, 'Successfully verified Databox settings!']
-		end
+      resp = service.receive_verification(fixtures[:config], payload)
+      resp.should == [true, 'Successfully verified Databox connection!']
+    end
 
-		it 'should fail upon unsuccessful api response' do
-			test = Faraday.new do |builder|
-				builder.adapter :test do |stub|
-					stub.get('/push/custom/3rglns26g76sws04') { [500, {}, ''] }
-				end
-			end
+    it 'should fail upon unsuccessful API response' do
+      test = Faraday.new do |builder|
+        builder.adapter :test do |stub|
+          stub.get(service_push_url) { [500, {}, ''] }
+        end
+      end
 
-			service.should_receive(:http_get)
-				.with('https://app.databox.com/push/custom/3rglns26g76sws04/logs')
-				.and_return(test.get('/push/custom/3rglns26g76sws04'))
+      service.should_receive(:http_get)
+        .with("#{fixtures[:config][:push_url]}/logs")
+        .and_return(test.get(service_push_url))
 
-			resp = service.receive_verification(config, payload)
-			resp.should == [false, 'Oops! Please check your settings again.']
-		end
-	end
+      resp = service.receive_verification(fixtures[:config], payload)
+      resp.should == [false, 'Oops! Please check your settings again.']
+    end
+  end
 
-	describe :receive_issue_impact_change do
-		let(:service) { Service::Databox.new('push', {}) }
-    	let(:config) do
-    	{
-    		:api_key => testdata[:api_key],
-    		:push_data_url => testdata[:push_data_url]
-    	}
-    	end
-		let(:payload) do
-		{
-				:title => 'foo title',
-				:impact_level => 1,
-				:impacted_devices_count => 1,
-				:crashes_count => 1,
-				:app => {
-					:name => 'foo name',
-					:bundle_identifier => 'foo.bar.baz'
-				}
-		}
-		end
+  describe :receive_issue_impact_change do
+    let(:service) { Service::Databox.new('push', {}) }
+    let(:payload) do
+      {
+        title:                  'issue title',
+        method:                 'method name',
+        impact_level:           1,
+        impacted_devices_count: 1,
+        crashes_count:          1,
+        app: {
+          name:              'app name',
+          bundle_identifier: 'foo.bar.baz',
+          platform:          'ios'
+        },
+        url: 'http://foo.com/bar'
+      }
+    end
 
-		it 'should respond to receive_issue_impact_change' do
-			service.respond_to?(:receive_issue_impact_change)
-		end
+    it 'should respond to receive_issue_impact_change' do
+      service.respond_to?(:receive_issue_impact_change)
+    end
 
-		it 'should succeed upon successful api response' do
-			test = Faraday.new do |builder|
-				builder.adapter :test do |stub|
-					stub.post('/push/custom/3rglns26g76sws04') { [200, {}, { response: { type: 'success', message: 'Items stored: 1' } }.to_json] }
-				end
-			end
+    it 'should succeed upon successful API response' do
+      test = Faraday.new do |builder|
+        builder.adapter :test do |stub|
+          stub.post(service_push_url) { [200, {}, fixtures[:response]] }
+        end
+      end
 
-			service.should_receive(:http_post)
-				.with('https://app.databox.com/push/custom/3rglns26g76sws04')
-				.and_return(test.post('/push/custom/3rglns26g76sws04'))
+      service.should_receive(:http_post)
+        .with(fixtures[:config][:push_url])
+        .and_return(test.post(service_push_url))
 
-			resp = service.receive_issue_impact_change(config, payload)
-			resp.should == :no_resource
-		end
+      resp = service.receive_issue_impact_change(fixtures[:config], payload)
+      resp.should == :no_resource
+    end
 
-		it 'should fail upon unsuccessful api response' do
-			test = Faraday.new do |builder|
-				builder.adapter :test do |stub|
-					stub.post('/push/custom/3rglns26g76sws04') { [500, {}, { response: { type: 'success', message: 'Items stored: 1' } }.to_json] }
-				end
-			end
+    it 'should fail upon unsuccessful API response' do
+      test = Faraday.new do |builder|
+        builder.adapter :test do |stub|
+          stub.post(service_push_url) { [500, {}, fixtures[:response]] }
+        end
+      end
 
-			service.should_receive(:http_post)
-				.with('https://app.databox.com/push/custom/3rglns26g76sws04')
-				.and_return(test.post('/push/custom/3rglns26g76sws04'))
+      service.should_receive(:http_post)
+        .with(fixtures[:config][:push_url])
+        .and_return(test.post(service_push_url))
 
-			lambda { service.receive_issue_impact_change(config, payload) }.should raise_error
-		end
-	end
+      -> { service.receive_issue_impact_change(fixtures[:config], payload) }
+        .should raise_error
+    end
+  end
 end
